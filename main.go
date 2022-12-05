42a3565fcc1212b24bc804dd9b277ea2427e7909
Sophia Allen <soph.m.allen@gmail.com>
2022-12-04 20:45:06 -0800

    add comment, run fmt

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
 	"aoc_template_go/template"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 type Solvable interface {
@@ -14,9 +14,11 @@ type Solvable interface {
 
 var solutions = map[int]Solvable{
 	0: template.Solution{},
+	// add each day following the pattern above
 }
 
 func main() {
+	// change day value to switch the solution that runs
 	const day = 1
 	solution := solutions[day]
 	fileScanner, reader := openFile(solution.GetDataPath())
@@ -27,9 +29,9 @@ func main() {
 func openFile(file string) (*bufio.Scanner, *os.File) {
 	readFile, err := os.Open(file)
 	if err != nil {
-        fmt.Println(err)
-    }
-    fileScanner := bufio.NewScanner(readFile)
- 	fileScanner.Split(bufio.ScanLines)
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
 	return fileScanner, readFile
 }
